86cbdd7cb59340538bb2678509e4c669b8b89f93
Alyssa <alynicbru@gmail.com>
2024-02-25 18:37:19 +1300

    Score gutterball frames

--- score.test.js
+++ score.test.js
@@ -6,3 +6,10 @@ test('test setup working', () => {
   expect(true).toBeTruthy()
 })
 
+test('scores a gutterball frame', () => {
+    const frame = [0, 0]
+    const expected = 0
+    const actual = score.scoreFrame(frame)
+  
+    expect(actual).toBe(expected)
+  })
